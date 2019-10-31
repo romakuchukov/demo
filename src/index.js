2530c7b522c48ff0a678fa387c5ff76d75b9eb4c
Roma K <romankuchukov@gmail.com>
2019-10-31 12:23:29 -0400

    reordered imports and removed visualizer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import allReducers from './reducers';
 
+import allReducers from './reducers';
 import { AppProvider } from './context/AppContext';
+
+
+import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
 let store = createStore(
     allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(<Provider store={store}><AppProvider><App/></AppProvider></Provider>, document.getElementById('root'));
