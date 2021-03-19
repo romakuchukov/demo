4e09cde9cb57c0162eba3bbf2a0268f4cb1d7076
Roma K <804157+romakuchukov@users.noreply.github.com>
2021-03-19 19:45:38 -0400

    indentation + removed info provider

--- src/ActiveStep.js
+++ src/ActiveStep.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { Button, Popover, Typography, makeStyles } from '@material-ui/core';
 
@@ -7,7 +7,7 @@ import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
 import Review      from './Review';
 
-import { AppContext, InfoProvider } from './context';
+import { AppContext } from './context';
 
 const useStyles = makeStyles(theme => ({
     stepper: {
@@ -26,47 +26,47 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const getStepContent = (step) => {
-    switch (step) {
-      case 0: return <ProductList />;
-      case 1: return <AddressForm />;
-      case 2: return <PaymentForm />;
-      case 3: return <Review />;
-      default:throw new Error('Unknown step');
-    }
+  switch (step) {
+    case 0: return <ProductList />;
+    case 1: return <AddressForm />;
+    case 2: return <PaymentForm />;
+    case 3: return <Review />;
+    default:throw new Error('Unknown step');
+  }
 }
 
 const ActiveStep = ({steps, activeStep, setActiveStep}) => {
 
-    const { buttons, button, typography } = useStyles();
+  const { buttons, button, typography } = useStyles();
 
-    const [shoppingbag] = React.useContext(AppContext);
-    const [anchorEl, setAnchorEl] = React.useState(null);
+  const [shoppingbag] = React.useContext(AppContext);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-    const handleClose = () => setAnchorEl(null);
+  const handleClose = () => setAnchorEl(null);
 
-    const open = Boolean(anchorEl);
-    const popOverID = open ? 'simple-popover' : undefined;
+  const open = Boolean(anchorEl);
+  const popOverID = open ? 'simple-popover' : undefined;
 
-    const handleNext = (event) => {
-        (!shoppingbag.itemCounter) ? setAnchorEl(event.currentTarget) : setActiveStep(activeStep + 1);
-    };
+  const handleNext = (event) => {
+      (!shoppingbag.itemCounter) ? setAnchorEl(event.currentTarget) : setActiveStep(activeStep + 1);
+  };
 
-    const handleBack = () => setActiveStep(activeStep - 1);
+  const handleBack = () => setActiveStep(activeStep - 1);
 
-    return(
-        <InfoProvider>
-            {getStepContent(activeStep)}
-            <div className={buttons}>
-                {activeStep !== 0 && (<Button onClick={handleBack} className={button}>Back</Button>)}
-                <Button variant="contained" color="primary" onClick={handleNext} className={button}>
-                    {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                </Button>
-                <Popover id={popOverID} open={open} anchorEl={anchorEl} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} transformOrigin={{ vertical: 'top', horizontal: 'right' }}>
-                    <Typography className={typography}>Please add at least one bike.</Typography>
-                </Popover>
-            </div>
-        </InfoProvider>
-    );
+  return(
+    <Fragment>
+      {getStepContent(activeStep)}
+      <div className={buttons}>
+        {activeStep !== 0 && (<Button onClick={handleBack} className={button}>Back</Button>)}
+        <Button variant="contained" color="primary" onClick={handleNext} className={button}>
+          {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+        </Button>
+        <Popover id={popOverID} open={open} anchorEl={anchorEl} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} transformOrigin={{ vertical: 'top', horizontal: 'right' }}>
+          <Typography className={typography}>Please add at least one bike.</Typography>
+        </Popover>
+      </div>
+    </Fragment>
+  );
 }
 
 export default ActiveStep;
