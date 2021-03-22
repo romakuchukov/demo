d1e0fb94498ecc3cdb8f1bff21d4be95b653fe30
Roma K <804157+romakuchukov@users.noreply.github.com>
2021-03-22 00:54:38 -0400

    added data to store

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -4,7 +4,15 @@ export const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
 
-  const [store, setStore] = React.useState({ itemCounter: 0 });
+  const [store, setStore] = React.useState({
+    1:{ counter: 0, product: null },
+    2:{ counter: 0, product: null },
+    3:{ counter: 0, product: null },
+    4:{ counter: 0, product: null },
+    5:{ counter: 0, product: null },
+    6:{ counter: 0, product: null },
+    itemCounter: 0
+  });
 
   return <AppContext.Provider value={[store, setStore]}>{children}</AppContext.Provider>;
 };
