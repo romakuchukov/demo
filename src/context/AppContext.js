10c8c9ee33fbc41c2290360261357136f12cab26
Roma K <804157+romakuchukov@users.noreply.github.com>
2021-03-23 21:56:04 -0400

    added inc and dec to contect obj

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,14 +5,14 @@ export const AppContext = React.createContext();
 export const AppProvider = ({ children }) => {
 
   const [store, setStore] = React.useState({
-    1:{ counter: 0, product: null },
-    2:{ counter: 0, product: null },
-    3:{ counter: 0, product: null },
-    4:{ counter: 0, product: null },
-    5:{ counter: 0, product: null },
-    6:{ counter: 0, product: null },
+    1: { counter: 0, product: null, increment: false, decrement: true },
+    2: { counter: 0, product: null, increment: false, decrement: true },
+    3: { counter: 0, product: null, increment: false, decrement: true },
+    4: { counter: 0, product: null, increment: true, decrement: true },
+    5: { counter: 0, product: null, increment: true, decrement: true },
+    6: { counter: 0, product: null, increment: true, decrement: true },
     itemCounter: 0
-  });
+});
 
   return <AppContext.Provider value={[store, setStore]}>{children}</AppContext.Provider>;
 };
